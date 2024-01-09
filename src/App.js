474650dad24d3f10ed266b173e26d7262ff9c1be
chaigit6 <chaithru2002@gmail.com>
2024-01-09 11:06:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Dropzone,
+  FileMosaic,
+  FullScreen,
+  ImagePreview,
+  VideoPreview
+} from "@files-ui/react";
+import * as React from "react";
 
-function App() {
+const BASE_URL = "https://www.myserver.com";
+
+export default function AdvancedDropzoneDemo() {
+  const [extFiles, setExtFiles] = React.useState([]);
+  const [imageSrc, setImageSrc] = React.useState(undefined);
+  const [videoSrc, setVideoSrc] = React.useState(undefined);
+
+  const updateFiles = (incommingFiles) => {
+    console.log("incomming files", incommingFiles);
+    setExtFiles(incommingFiles);
+  };
+  const onDelete = (id) => {
+    setExtFiles(extFiles.filter((x) => x.id !== id));
+  };
+  const handleSee = (imageSource) => {
+    setImageSrc(imageSource);
+  };
+  const handleWatch = (videoSource) => {
+    setVideoSrc(videoSource);
+  };
+  const handleStart = (filesToUpload) => {
+    console.log("advanced demo start upload", filesToUpload);
+  };
+  const handleFinish = (uploadedFiles) => {
+    console.log("advanced demo finish upload", uploadedFiles);
+  };
+  const handleAbort = (id) => {
+    setExtFiles(
+      extFiles.map((ef) => {
+        if (ef.id === id) {
+          return { ...ef, uploadStatus: "aborted" };
+        } else return { ...ef };
+      })
+    );
+  };
+  const handleCancel = (id) => {
+    setExtFiles(
+      extFiles.map((ef) => {
+        if (ef.id === id) {
+          return { ...ef, uploadStatus: undefined };
+        } else return { ...ef };
+      })
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Dropzone
+        onChange={updateFiles}
+        minHeight="195px"
+        value={extFiles}
+        accept="image/, video/"
+        maxFiles={3}
+        maxFileSize={2 * 1024 * 1024}
+        label="Drag'n drop files here or click to browse"
+        uploadConfig={{
+          // autoUpload: true
+          url: BASE_URL + "/file",
+          cleanOnUpload: true
+        }}
+        onUploadStart={handleStart}
+        onUploadFinish={handleFinish}
+        fakeUpload
+        actionButtons={{
+          position: "after",
+          abortButton: {},
+          deleteButton: {},
+          uploadButton: {}
+        }}
+      >
+        {extFiles.map((file) => (
+          <FileMosaic
+            {...file}
+            key={file.id}
+            onDelete={onDelete}
+            onSee={handleSee}
+            onWatch={handleWatch}
+            onAbort={handleAbort}
+            onCancel={handleCancel}
+            resultOnTooltip
+            alwaysActive
+            preview
+            info
+          />
+        ))}
+      </Dropzone>
+      <FullScreen
+        open={imageSrc !== undefined}
+        onClose={() => setImageSrc(undefined)}
+      >
+        <ImagePreview src={imageSrc} />
+      </FullScreen>
+      <FullScreen
+        open={videoSrc !== undefined}
+        onClose={() => setVideoSrc(undefined)}
+      >
+        <VideoPreview src={videoSrc} autoPlay controls />
+      </FullScreen>
+    </>
   );
 }
-
-export default App;
